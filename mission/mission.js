a7dedf166e76ea8df3aa275eb3d5ab55ff48ef1d
natemasca <mas23040@byui.edu>
2025-09-19 07:59:23 -0600

    added dark theme

--- mission/mission.js
+++ mission/mission.js
@@ -1,4 +1,16 @@
-const apples = 10;
-const oranges = 5;
+const themeSelector = document.querySelector("select") // replace with code to select dropdown element out of the HTML (Hint: document.querySelector)
+function changeTheme() {
+// check to see what the current value of our select is.
+// The current value is conveniently found in themeSelector.value!
 
-console.log(apples + oranges);
+// if the value is dark then:
+// add the dark class to the body
+// change the source of the logo img to point to the white logo.
+// otherwise
+// remove the dark class
+// make sure the logo src is the blue logo.
+}
+
+// add an event listener to the themeSelector element here.
+// Use the changeTheme function as the event handler function.
+themeSelector.addEventListener('change', changeTheme);
